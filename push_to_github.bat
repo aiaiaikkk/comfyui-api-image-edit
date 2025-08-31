acad21b215d7089066ea40f444b50b70881120db
aiaiaikkk <1005937113@qq.com>
2025-09-01 04:47:15 +0800

    Create push_to_github.bat

---
+++ push_to_github.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\86181\OneDrive\文档\GitHub\ComfyUI_API_Image_Edit"
+echo Opening GitHub Desktop...
+start "" "C:\Users\%USERNAME%\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --open-repo "%CD%"
+pause
